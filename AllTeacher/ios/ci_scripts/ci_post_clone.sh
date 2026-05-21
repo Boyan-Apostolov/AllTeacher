c4bd88f2feed54b25cfb50c7415ba8b3409039e0
Boyan Apostolov <boian4934@gmail.com>
2026-05-21 23:57:09 +0200

    fix: add npm ci before pod install in xcode cloud script

--- AllTeacher/ios/ci_scripts/ci_post_clone.sh
+++ AllTeacher/ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -e
 
-cd /Volumes/workspace/repository/AllTeacher/ios/ios
+# Install Node
+brew install node@20
+brew link node@20 --force
+
+# Install JS dependencies
+cd /Volumes/workspace/repository/AllTeacher/ios
+npm ci
+
+# Install iOS native dependencies
+cd ios
 pod install
