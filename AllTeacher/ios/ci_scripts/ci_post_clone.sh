d5d44012b95153be0f9fb9311df75ad6893aa8db
Boyan Apostolov <boian4934@gmail.com>
2026-05-21 23:52:05 +0200

    fix: remove rm -rf, use pod install in xcode cloud script

--- AllTeacher/ios/ci_scripts/ci_post_clone.sh
+++ AllTeacher/ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
 
-brew install node@20
-brew link node@20 --force
-
-cd /Volumes/workspace/repository/AllTeacher/ios
-npm ci
-CI=1 npx expo prebuild --platform ios --no-install
+cd /Volumes/workspace/repository/AllTeacher/ios/ios
+pod install
